843e7ff18c20de03e0a8ba5347f0d517558e9955
Ben Tagtow <bentagtow@gmail.com>
2020-02-12 17:41:40 -0700

    Adds individual Shelter details to Shelter page.

--- shelter.js
+++ shelter.js
@@ -1,37 +1,46 @@
-const shelterCard = document.querySelector('shelter-card')
+const shelterContainer = document.querySelector(".shelter-container") 
+const petsLink = document.querySelector("#browse-pets")
 const home = document.querySelector('#home')
 const currentUser = document.querySelector("#current-users")
 const currentUserSelect = document.querySelector("#current-user-select")
 const currentUserSelectContainer = document.querySelector("#current-user-select-container")
-
 const userDropDown = document.querySelector('#user-dropdown')
-
-
-console.log(shelterCard)
+const createUser = document.querySelector("#create-user")
+const newUserNav = document.querySelector("#new-user-nav")
+const userPageButton = document.querySelector("#go-to-user-page")
 
 document.addEventListener('DOMContentLoaded', () => {
     home.addEventListener("click", goHome)
-    currentUser.addEventListener('click', () => {
-        createUserOptions()
-        appendUserOptions(userOption)
-    })
+    userPageButton.addEventListener("click", goToUserPage)
+    createUserOptions()
+    petsLink.addEventListener('click', goToAvailablePets)
     
 })
 
 fetch('http://localhost:3000/shelters')
     .then(response => response.json())
-    .then(shelters => {
-        shelterDetails(shelters)
-        
-    })
+    .then(shelterDetails)
 
 
 
 function shelterDetails(shelters){
-    shelters.map(shelter => {
-        
-    })
+    shelters.map(
+        getShelterName
+    )
 }
+
+function getShelterName(shelter){
+    let shelterCard = document.createElement('shelter-card')
+    let shelterName = document.createElement('h3')
+    shelterName.innerText = shelter.name 
+    shelterCard.className = 'shelter-details'
+    shelterCard.append(shelterName)
+    let shelterLink = document.createElement('h3')
+    shelterLink.innerText = shelter.link 
+    shelterContainer.append(shelterCard)
+    shelterCard.append(shelterLink)
+}
+
 function goHome(){
     window.location = "http://localhost:3001/"
 }
@@ -48,15 +57,31 @@ function createUserOptions(){
 }
 
 
+function goToUserPage(){
+  window.location = `http://localhost:3001/user.html?id=${userDropDown.value}`
+}
+
+function goToShelterPage(){
+  window.location = `http://localhost:3001/shelter.html`
+}
+
+function goToAvailablePets(){
+  window.location = `http://localhost:3001/pets.html`
+}
+
+
 currentUser.addEventListener("click", () => {
     hideElement(currentUserSelect)
   })
+
+newUserNav.addEventListener("click", () => {
+    hideElement(createUser)
+})
+
   
   
 function hideElement(element) {
     if (element.style.display === "none") {
     element.style.display = "block";
-    } else {
-    element.style.display = "none";
     }
 }
